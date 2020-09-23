17cb0a774d599b85e7bf42a6c511541045b5ed88
robson.rocha <robson.rocha@sonda.com>
2020-09-23 14:01:03 -0300

    categoria resource

---
+++ src/main/java/br/com/cbmerj/inventory/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package br.com.cbmerj.inventory.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cbmerj.inventory.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> lista() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
+	}
+
+}
