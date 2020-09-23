2f5c39883d12a5f3b450fb085d5725702191a8ad
robson.rocha <robson.rocha@sonda.com>
2020-09-23 15:29:42 -0300

    categoria repository e service

---
+++ src/main/java/br/com/cbmerj/inventory/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package br.com.cbmerj.inventory.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cbmerj.inventory.domain.Categoria;
+import br.com.cbmerj.inventory.repositories.CategoriaRepository;
+import br.com.cbmerj.inventory.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+}
