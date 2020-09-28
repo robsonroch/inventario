71302be1d06e7881855bcd34df8d6224a0558db4
robson.rocha <robson.rocha@sonda.com>
2020-09-28 20:24:17 -0300

    cidade e estado

--- src/main/java/br/com/cbmerj/inventory/InventoryApplication.java
+++ src/main/java/br/com/cbmerj/inventory/InventoryApplication.java
@@ -1,7 +1,6 @@
 package br.com.cbmerj.inventory;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.cbmerj.inventory.domain.Categoria;
+import br.com.cbmerj.inventory.domain.Cidade;
+import br.com.cbmerj.inventory.domain.Estado;
 import br.com.cbmerj.inventory.domain.Produto;
 import br.com.cbmerj.inventory.repositories.CategoriaRepository;
+import br.com.cbmerj.inventory.repositories.CidadeRepository;
+import br.com.cbmerj.inventory.repositories.EstadoRepository;
 import br.com.cbmerj.inventory.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +23,10 @@ public class InventoryApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
@@ -41,8 +48,23 @@ public class InventoryApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-				
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+				
 	}	
 }
